ee5ac6a39a002771e1eec389cb01162e2d2b2af4
zbq <2236629281@qq.com>
2019-07-29 13:19:54 +0800

    添加--添加model层主键特性

---
+++ ORMProject/EntitySQLMapping/ORM/ObjectRelation/Key.cs
@@ -0,0 +1,29 @@
+﻿/************************************************************************************
+* Copyright (c) 2019 All Rights Reserved.
+*命名空间：EntitySQLMapping.ORM.ObjectRelation
+*文件名： Key
+*创建人： 郑伯强
+*创建时间：2019/7/28 11:31:23
+*描述
+*=====================================================================
+*修改标记
+*修改时间：2019/7/28 11:31:23
+*修改人：郑伯强
+*描述：
+************************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntitySQLMapping.ORM.ObjectRelation
+{
+    /// <summary>
+    /// 主键
+    /// </summary>
+    public class KeyAttribute : Attribute
+    {
+        //TODO: 主键标识
+    }
+}
